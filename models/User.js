27523ec07b7ad75b31136c8771fbf4dfa5432e2f
Damian Diaz <damiandiaz0509@gmail.com>
2023-07-12 15:24:41 -0400

    made a few changes to the User model

--- models/User.js
+++ models/User.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.belongsToMany (Album, {through: 'UserAlbum'})
     }
   }
   User.init({
