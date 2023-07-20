d14a061e95621d1495bdb3e7914b2d5fe7a05612
Damian Diaz <damiandiaz0509@gmail.com>
2023-07-20 16:11:07 -0400

    albums are now viewable on the site.  Still need to write the code to allow the user to save albums and specific songs to their profile.  Added some styling to make the page pop and added some cool buttons that ill eventually use in order to allow the user to save the albums and songs off of the page

--- controllers/album.js
+++ controllers/album.js
@@ -1,10 +1,12 @@
 const { Profile } = require('../models');
+const {Album} = require("../models")
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 
-const albumGet = (req, res) => {
-    res.render('album', {title: "My Favorite Albums"})
+const albumGet = async (req, res) => {
+  const albums = await Album.findAll({attributes: { exclude: ['ArtistId'] }})
+    res.render('album', {title: "Albums", albums})
   }
 
   module.exports = {
--- seeders/20230718031113-artistSeed.js
+++ seeders/20230718031113-artistSeed.js
@@ -1,22 +1,21 @@
-// 'use strict';
+'use strict';
 
-// // When running the seeder command it needs to be name of Database table and not the models files! 
+// When running the seeder command it needs to be name of Database table and not the models files! 
 
-// /** @type {import('sequelize-cli').Migration} */
-// module.exports = {
-//   async up (queryInterface, Sequelize) {
-//     return queryInterface.bulkInsert('Artists', [{
-//     artistName: 'test Artist Name',
-//     artistImage: 'test Artist image',
-//     artistAge: 27,
-//     genreID: 1,
-//     songID: 1,
-//     profileID: 1,
-//     createdAt: new Date(),
-//     updatedAt: new Date() 
-//   }]);
-// },
-// down: (queryInterface, Sequelize) => {
-//   return queryInterface.bulkDelete('Artists', null, {});
-//   }
-// };
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Artists', [{
+    artistName: 'test Artist Name',
+    artistImage: 'test Artist image',
+    artistAge: 27,
+    songID: 1,
+    profileID: 1,
+    createdAt: new Date(),
+    updatedAt: new Date() 
+  }]);
+},
+down: (queryInterface, Sequelize) => {
+  return queryInterface.bulkDelete('Artists', null, {});
+  }
+};
