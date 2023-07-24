d3644f198010ac49203426ffcb90e4aff9f9ca33
Damian Diaz <damiandiaz0509@gmail.com>
2023-07-24 17:05:13 -0400

    Changed the headers so that the correct user is displayed upon visiting the edit and delete routes.  Added an additonal header to bring the user that is logged in to their profile rather than hard coding the id value into the url.  Fixed the delete route so that it also deletes the favorite albums off of the ProfileAlbums table.  Also added some bootstrap modals that will display messages to the user dependign on the album they try to favorite

--- controllers/album.js
+++ controllers/album.js
@@ -21,66 +21,53 @@ const albumGet = async (req, res) => {
   }
   
   const albumPost = async (req, res) => {
-      const userProfileID = req.user.id;
-      console.log('User Profile ID:', userProfileID);
-      // res.render('album', {title: "Albums", albums, userProfileID})
-      try {
-        const { albumID, profileID } = req.body;
-    
-        // Find the user's profile by ID
-        const userProfile = await Profile.findByPk(profileID);
-        if (!userProfile) {
-          return res.status(404).send('User profile not found.');
-        }
-    
-        // Find the album by ID
-        const album = await Album.findByPk(albumID, {
-          attributes: ['id', 'albumName', 'albumCover'], // Include only the required attributes
-        });
-        if (!album) {
-          return res.status(404).send('Album not found.');
-        }
-    
-        // Check if the association already exists
-        // Goes through the ProfileAlbum table and looks to see if a user at a given id has already favorited an album with that specific id
-        const existingAssociation = await ProfileAlbum.findOne({
-          where: {
-            profileID: userProfile.id,
-            albumID: album.id,
-          },
-        });
-    
-        if (existingAssociation) {
-          // The association already exists, do not create a duplicate
-          return res.status(200).json ({
-            status: 'error',
-          message: 'Album already in favorites.'});
-        }
-    
-        // Create the association between the profile and the album
-        // Create a new entry in the table if everything goes well, setting profileID in the ProfileAlbum table to the user that is logged in and the albumID to whatever album they just favorited
-        await ProfileAlbum.create({
-          profileID: userProfile.id, // Set the profileId foreign key
-          albumID: album.id, // Set the albumId foreign key
-          albumName: album.albumName, // Set the album name
-          albumCover: album.albumCover, // Set the album cover
-        });
-    
-  
-        // Changing the messages to json so that the errors and successes are able to caught by the ejs template, allowing for certain popups rather the navigating the user to another page before they are done favoriting the albums
-        return res.status(200).json({
-          status: 'success',
-          message: 'Album successfully added to favorites!',
-        });
-      } catch (error) {
-        console.error('Error while adding album to favorites:', error);
-        return res.status(500).json({
-          status: 'error',
-          message: 'An error occurred while adding the album to favorites.',
-        });
+  const userProfileID = req.user.id;
+  console.log('User Profile ID:', userProfileID);
+  try {
+    const { albumID, profileID } = req.body;
 
+    // Find the user's profile by ID
+    const userProfile = await Profile.findByPk(profileID);
+    if (!userProfile) {
+      return res.status(404).json({ error: 'User profile not found.' });
+    }
+
+    // Find the album by ID
+    const album = await Album.findByPk(albumID, {
+      attributes: ['id', 'albumName', 'albumCover'], // Include only the required attributes
+    });
+    if (!album) {
+      return res.status(404).json({ error: 'Album not found.' });
+    }
+
+    // Check if the association already exists
+    const existingAssociation = await ProfileAlbum.findOne({
+      where: {
+        profileID: userProfile.id,
+        albumID: album.id,
+      },
+    });
+
+    if (existingAssociation) {
+      // The association already exists, do not create a duplicate
+      return res.status(409).json({ error: 'Album already in favorites.' });
+    }
+
+    // Create the association between the profile and the album
+    await ProfileAlbum.create({
+      profileID: userProfile.id,
+      albumID: album.id,
+      albumName: album.albumName,
+      albumCover: album.albumCover,
+    });
+
+    return res.status(200).json({ message: 'Album successfully added to favorites!' });
+  } catch (error) {
+    console.error('Error while adding album to favorites:', error);
+    return res.status(500).json({ error: 'An error occurred while adding the album to favorites.' });
   }
-}
+};
+
 
 
 
