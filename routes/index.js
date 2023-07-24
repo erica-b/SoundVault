f7500df78c675723b738b5feb9256dac490598bc
Damian Diaz <damiandiaz0509@gmail.com>
2023-07-24 15:54:08 -0400

    had some trouble merging.  Fixed now

--- routes/index.js
+++ routes/index.js
@@ -19,10 +19,7 @@ const songController = require('../controllers/song');
 
  /* Album Router */
  // get album ejs template at this route url
- router.get('/album', authCheck.authCheck, albumController.albumGet)
-// post album from search bar
- router.post('/album/search', authCheck.authCheck, albumController.albumPost)
-
+ router.get('/album', authCheck.authCheck ,albumController.albumGet)
 
  router.post('/album',authCheck.authCheck,albumController.albumPost)
 
@@ -30,10 +27,6 @@ const songController = require('../controllers/song');
 
 router.get('/album/favorites', authCheck.authCheck ,albumController.albumFavsGet)
  
-
-router.get('/album/favorites', authCheck.authCheck, albumController.albumFavsGet)
- router.post('/album/favorites', albumController.albumFavsPost)
-
 
  /* Artist Router */
  // get artist ejs template at this route url
@@ -48,11 +41,7 @@ router.get('/album/favorites', authCheck.authCheck, albumController.albumFavsGet
  // get song ejs template at this route url
  router.get('/song', authCheck.authCheck, songController.songGet)
  // post song from search bar
-
  router.post('/song/search', authCheck.authCheck ,songController.songPost)
-
- router.post('/song/search', authCheck.authCheck, songController.songPost)
-
 
 
 
