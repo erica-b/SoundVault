ab7af671bb9c81fc9d16fb85c6c9f16f99af68b8
Damian Diaz <damiandiaz0509@gmail.com>
2023-08-22 19:50:39 -0400

    still doing the same stuff with render

--- config/database.js
+++ config/database.js
@@ -9,12 +9,13 @@ const dbPass = process.env.DB_PASS;
 const dbHost = process.env.DB_HOST;
 
 const sequelize = new Sequelize(dbName, dbUser, dbPass, {
-    host: dbHost,
-    dialect: 'postgres',
-    ssl: true,
-    dialectOptions: {
-      "ssl": {"require":true }
-    }
-  });
+  host: dbHost,
+  dialect: 'postgres',
+  ssl: true,
+  dialectOptions: {
+    ssl: { require: true }
+  },
+  logging: console.log // Add this line
+});
   
   module.exports = sequelize;
