eeb1efa5613b7440d8af39ca8f6748490e0fd06a
Damian Diaz <damiandiaz0509@gmail.com>
2023-08-24 19:22:51 -0400

    going back to what worked

--- config/database.js
+++ config/database.js
@@ -1,18 +1,8 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
+const {Sequelize} = require ('sequelize')
+require('dotenv').config()
 
-const databaseName = 'sufdxojz'; // Replace with your actual database name
-const databaseURL = process.env.DATABASE_URL || `postgres://localhost:5433/${databaseName}`;
 
-const sequelize = new Sequelize(databaseURL, {
-  logging: false,
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: process.env.DATABASE_URL ? {
-      require: true,
-      rejectUnauthorized: false
-    } : false
-  }
-});
+const dbURL = process.env.DB_URL;
 
-module.exports = sequelize;
+
+const sequelize = new Sequelize(dbURL)
