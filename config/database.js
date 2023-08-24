ae77884a097e8659ecf66ac73b0292703daec006
Damian Diaz <damiandiaz0509@gmail.com>
2023-08-24 19:18:48 -0400

    adf

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+const databaseName = 'sufdxojz'; // Replace with your actual database name
 const databaseURL = process.env.DATABASE_URL || `postgres://localhost:5433/${databaseName}`;
 
 const sequelize = new Sequelize(databaseURL, {
